d4180a2dc72eab82c2150a19a5f6925b7dee4d74
MarcusPFF <marcus.pff03@gmail.com>
2024-11-23 00:48:01 +0100

    FileIO loadUserData & saveUserData

--- src/StreamingProg/FileIO.java
+++ src/StreamingProg/FileIO.java
@@ -1,19 +1,58 @@
 package StreamingProg;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 public class FileIO {
+    private User user;
+    private String path = "data/userData/userData.txt";
 
-    public HashMap<String, User> loadUserData(String path) {
-        HashMap<String, User> userData = new HashMap<>();
+    public FileIO(User user, String path) {
+        this.user = user;
+        this.path = path;
+    }
 
+    public HashMap<String, Integer> loadUserData(String path) {
+        HashMap<String, Integer> userData = new HashMap<>();
+        String username;
+        String password;
+        File file = new File(path);
+        try {
+            Scanner scanner = new Scanner(file);
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(",");
+                if (data.length == 3) {
+                    username = data[0].trim();
+                    password = data[1].trim();
+                    boolean isAdmin = Boolean.parseBoolean(data[2].trim());
+                    userData.put(username, Integer.parseInt(password));
+                    if (isAdmin) {
+                        System.out.println("Bruger: " + username + " har admin rettigheder");
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Ingen fil fundet");
+        }
         return userData;
     }
 
-    public void saveUserData(String path, HashMap<String, User> userData) {
-
+    public void saveUserData(String path, HashMap<String, Integer> userData) {
+        String username = user.getUsername();
+        File file = new File(path);
+        try {
+            FileWriter writer = new FileWriter(username + "watchedMovies.txt");
+        } catch (IOException e) {
+            System.out.println("Fejlede med skrive til fil");
+        }
     }
 
 
@@ -27,11 +66,11 @@ public class FileIO {
         return seriesList;
     }
 
-    public void addMedia(){
+    public void addMedia() {
 
     }
 
-    public void removeMedia(){
+    public void removeMedia() {
 
     }
 }
