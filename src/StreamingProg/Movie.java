bf89cc9075235eefff8313d616a2d98eca777e91
JonasOutzen <jonas.outzen.j@gmail.com>
2024-11-25 11:33:49 +0100

    netflix, fileIO og media

--- src/StreamingProg/Movie.java
+++ src/StreamingProg/Movie.java
@@ -3,23 +3,14 @@ package StreamingProg;
 import java.util.List;
 
 public class Movie extends Media {
-    private int duration;
 
-    public Movie(String title, String genre, int releaseYear, float rating, int duration) {
-        super(title, genre, releaseYear, rating);
-        this.duration = duration;
+
+    public Movie(String title, int releaseYear, String genre, float rating) {
+        super(title, releaseYear, genre, rating);
     }
 
-    public void playMovie(){
+    public void playMovie() {
         System.out.println(getTitle() + " afspilles nu...");
-
-    }
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
     }
 }
 
