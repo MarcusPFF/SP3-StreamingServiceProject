fe8746d2a53d5cae679a64aaeeaccf055415099d
JonasOutzen <jonas.outzen.j@gmail.com>
2024-11-26 12:24:40 +0100

    fixes

--- src/StreamingProg/Netflix.java
+++ src/StreamingProg/Netflix.java
@@ -6,32 +6,28 @@ import java.util.Scanner;
 
 public class Netflix {
 
-    private List<String> movieData;
-    private UserManager userManager;
-    private Menu menu = new Menu();
-    private TextUI ui = new TextUI();
+    UserManager userManager;
+    Menu menu;
+    TextUI ui;
 
-    public Netflix() {
-        this.userManager = new UserManager();
+    public Netflix(UserManager userManager, Menu menu, TextUI ui) {
+        this.userManager = userManager;
+        this.menu = menu;
+        this.ui = ui;
     }
+
 
     public void runApplication() {
-        ui.displayMsg("Velkommen til netflix-backend streamingtjeneste systemet");
-        runUserManager();
-        //runMenu();
-    }
-
-    public void runQuestionMark() {
         ArrayList<Media> Medias = new ArrayList<>();
         FileIO fileIO = new FileIO();
-        movieData = fileIO.readMovieData(fileIO.getMoviesDataPath());
+        List<String> movieData = fileIO.readMovieData("entertainmentData\\movies.txt");
 
-        for (int i = 0; i > movieData.size(); i++) {
+        for (int i = 0; i < movieData.size(); i++) {
             String[] movieInfo = movieData.get(i).split(";");
             String title = movieInfo[0];
             String releaseyear = movieInfo[1];
             String genre = movieInfo[2];
-            float rating = Float.parseFloat(movieInfo[3].replace(",", "."));
+            float rating = Float.parseFloat(movieInfo[3]);
 
             Movie movie = new Movie(title, releaseyear, genre, rating);
 
@@ -40,79 +36,54 @@ public class Netflix {
         }
     }
 
-    public List<String> getMovieData() {
-        return movieData;
-    }
-
-
     public void runUserManager() {
+        UserManager userManager = new UserManager();
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
         while (running) {
-            ui.displayMsg("--Vælg en mulighed--");
+            ui.displayMsg("\n Vælg en mulighed: ");
             ui.displayMsg("1. Lav ny bruger");
             ui.displayMsg("2. Login");
             ui.displayMsg("3. Slet bruger");
             ui.displayMsg("4. Vis alle brugere");
             ui.displayMsg("5. Exit");
+            ui.displayMsg("Vælg en mulighed");
 
             int choice = Integer.parseInt(scanner.nextLine());
 
             switch (choice) {
-                case 1: {
-                    ui.displayMsg("Lav ny bruger");
-                    String username = ui.promptText("Skriv dit brugernavn");
-                    String password = ui.promptText("Skriv password");
-                    boolean isAdmin = ui.promptBinary("Skal brugeren være admin? Y/N");
+                case 1:
+                    ui.displayMsg("Skriv brugernavn");
+                    String username = scanner.nextLine();
+                    ui.displayMsg("Skriv password");
+                    String password = scanner.nextLine();
+                    ui.displayMsg("Skal den her bruger være en admin?");
+                    boolean isAdmin = Boolean.parseBoolean(scanner.nextLine());
                     userManager.createUser(username, password, isAdmin);
                     break;
-                }
 
-                case 2: {
-                    ui.displayMsg("Login");
-                    String username = ui.promptText("Skriv brugernavn");
-                    String password = ui.promptText("Skriv password");
-                    userManager.validateUser(username, password);
+                case 2:
+                    ui.displayMsg("Skriv brugernavn");
+                    String validUsername = scanner.nextLine();
+                    ui.displayMsg("Skriv password");
+                    String validatePassword = scanner.nextLine();
+
+                    userManager.validateUser(validUsername, validatePassword);
                     break;
-                }
-
-                case 3: {
-                    ui.displayMsg("Slet bruger");
-                    String username = ui.promptText("Skriv det brugernavn, du vil slette");
-                    userManager.deleteUser(username);
+                case 3:
+                    ui.displayMsg("Skriv det brugernavn du vil slette");
+                    String deleteUsername = scanner.nextLine();
+                    userManager.deleteUser(deleteUsername);
                     break;
-                }
-
-                case 4: {
-                    ui.displayMsg("Vis alle brugere:");
-                    if (userManager.getUserData().isEmpty()) {
-                        ui.displayMsg("Ingen brugere fundet.");
-                    } else {
-                        userManager.getUserData().forEach((username, user) -> {
-                            ui.displayMsg("Brugernavn: " + username + ", Admin: " + (user.isAdmin() ? "Ja" : "Nej"));
-                        });
-                    }
-                    break;
-                }
-
-                case 5: {
-                    ui.displayMsg("Luk programmet ned");
+                case 4:
                     ui.displayMsg("Lukker ned...");
                     running = false;
                     break;
-                }
 
-                default: {
+                default:
                     ui.displayMsg("Forkert input. Prøv igen");
-                }
             }
-
         }
         scanner.close();
-    }
-
-    public void runMenu() {
-        Menu menu = new Menu();
-        menu.displayMenu();
     }
 }
